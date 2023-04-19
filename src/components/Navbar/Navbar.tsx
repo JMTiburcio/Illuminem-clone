8781f72800ff3ff4ccfb072f015ef0597172d353
JMTiburcio <jmrtme@gmail.com>
2023-04-18 23:31:56 -0300

    refactor: using BootStrap components for Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,42 +1,27 @@
-import React from "react";
-import Container from "../Container";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
 import Logo from "../Logo";
-import NavLink from "./NavLink";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import { useTheme } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-const Navbar: React.FC = () => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-
+export default function NextNavbar() {
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]">
-        <Container>
-          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-            <Logo />
-            {isSmallScreen ? (
-              <IconButton edge="end" color="inherit" aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-            ) : (
-              <>
-                <NavLink label="One Stop" selected={false} />
-                <NavLink label="Illuminem Voice" selected={false} />
-                <NavLink label="Explorer" selected={false} />
-                <NavLink label="Categories" selected={false} />
-                <NavLink label="NewsLetter" selected={false} />
-                <NavLink label="More" selected={false} />
-                <NavLink label="About" selected={false} />
-              </>
-            )}
-          </div>
-        </Container>
-      </div>
-    </div>
+    <Navbar collapseOnSelect expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
+          <Logo />
+        </Navbar.Brand>
+        {/* <Navbar.Toggle /> */}
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link href="/">One Stop</Nav.Link>
+            <Nav.Link href="/">Illuminem Voices</Nav.Link>
+            <Nav.Link href="/">Explore</Nav.Link>
+            <Nav.Link href="/">Categories</Nav.Link>
+            <Nav.Link href="/">Newsletter</Nav.Link>
+            <Nav.Link href="/">More</Nav.Link>
+            <Nav.Link href="/">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
-
-export default Navbar;
+}
