0a7324931fddc94a6ae488feaab4a1f50e5096ab
JMTiburcio <jmrtme@gmail.com>
2023-04-19 20:06:46 -0300

    feat: adding newsletter and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar/Navbar";
 import HomeBanner from "@/components/Banner/Banner";
 import MainNews from "@/components/MainNews/MainNews";
 import SecondaryNews from "@/components/SecondaryNews/SecondaryNews";
+import NewsLetter from "@/components/NewsLetter/NewsLetter";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
       <SecondaryNews title="Latest on Energy" />
       <SecondaryNews title="Latest on Sustainable Finance" />
       <SecondaryNews title="Latest on Sustainable Lifestyle" />
+      <NewsLetter />
+      <Footer />
     </div>
   );
 }
