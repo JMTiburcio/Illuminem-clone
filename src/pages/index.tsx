790383e539d4d86a37cfd1edbd6c573e518d5d73
JMTiburcio <jmrtme@gmail.com>
2023-04-18 23:33:44 -0300

    feat: add Banner and News components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import Navbar from "@/components/Navbar/Navbar";
+import HomeBanner from "@/components/Banner/Banner";
+import MainNews from "@/components/MainNews/MainNews";
+import SecondaryNews from "@/components/SecondaryNews/SecondaryNews";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <main>
-        <h1>Bem-vindo ao meu aplicativo Next.js</h1>
-      </main>
+      <HomeBanner />
+      <MainNews title="Latest on Sustaintability & Energy" />
+      <MainNews title="Most Trending" />
+      <SecondaryNews title="Latest on Climate Change" />
+      <SecondaryNews title="Latest on Energy" />
+      <SecondaryNews title="Latest on Sustainable Finance" />
+      <SecondaryNews title="Latest on Sustainable Lifestyle" />
     </div>
   );
 }
