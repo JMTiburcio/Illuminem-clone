40a88b8026c6461f75be9692c9a39b238ff2b581
JMTiburcio <jmrtme@gmail.com>
2023-04-20 09:52:08 -0300

    style: fix footer responsiveness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,14 +7,14 @@ const Footer = () => {
     <footer className="border-top mt-5 py-4">
       <Container>
         <Logo />
-        <Row>
+        <Row className="footer-row">
           <Col md={9}>
-            <div className="d-flex mt-4">
+            <div className="d-flex mt-4 footer-links">
               <h6>About us</h6>
-              <h6 className="pl-4">Terms & Conditions</h6>
-              <h6 className="pl-4">Privacy</h6>
-              <h6 className="pl-4">Cookies</h6>
-              <h6 className="pl-4">Contact Us</h6>
+              <h6>Terms & Conditions</h6>
+              <h6>Privacy</h6>
+              <h6>Cookies</h6>
+              <h6>Contact Us</h6>
             </div>
             <p className="text-muted small">
               © 2023 illuminem. All rights reserved. Various trademarks held by
@@ -22,22 +22,24 @@ const Footer = () => {
             </p>
           </Col>
           <Col md={3}>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.linkedin.com/company/energen-info/"
-              aria-label="LinkedIn"
-            >
-              <FaLinkedin size={28} />
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://twitter.com/illuminemNews"
-              aria-label="Twitter Link"
-            >
-              <FaTwitter size={28} />
-            </a>
+            <div className="footer-social-icons">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/company/energen-info/"
+                aria-label="LinkedIn"
+              >
+                <FaLinkedin size={28} />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://twitter.com/illuminemNews"
+                aria-label="Twitter Link"
+              >
+                <FaTwitter size={28} />
+              </a>
+            </div>
           </Col>
         </Row>
       </Container>
