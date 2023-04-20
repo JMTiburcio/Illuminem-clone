9c52c0410974a718417ee5b6a33096d3095c494a
JMTiburcio <jmrtme@gmail.com>
2023-04-20 09:07:10 -0300

    feat: adding toggle and dropdown

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,84 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
 import Logo from "../Logo";
 
 export default function NextNavbar() {
   return (
-    <Navbar collapseOnSelect expand="lg">
+    <Navbar sticky="top" collapseOnSelect expand="lg">
       <Container>
-        <Navbar.Brand href="/">
+        <Navbar.Brand className="mr-3" href="/">
           <Logo />
         </Navbar.Brand>
-        {/* <Navbar.Toggle /> */}
+        <Navbar.Toggle />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
-            <Nav.Link href="/">One Stop</Nav.Link>
-            <Nav.Link href="/">Illuminem Voices</Nav.Link>
-            <Nav.Link href="/">Explore</Nav.Link>
-            <Nav.Link href="/">Categories</Nav.Link>
-            <Nav.Link href="/">Newsletter</Nav.Link>
-            <Nav.Link href="/">More</Nav.Link>
-            <Nav.Link href="/">About</Nav.Link>
+            <Nav.Link className="mr-3" href="/">
+              One Stop
+            </Nav.Link>
+
+            <NavDropdown
+              menuVariant="dark"
+              title="Illuminem Voices"
+              id="collasible-nav-dropdown"
+              className="mr-3"
+            >
+              <NavDropdown.Item href="#action/3.1">
+                Thought Leaders
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Future Thought Leaders
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Our Authors
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link className="mr-3" href="/">
+              Explore
+            </Nav.Link>
+
+            <NavDropdown
+              menuVariant="dark"
+              title="Categories"
+              id="collasible-nav-dropdown"
+              className="mr-3"
+            >
+              <NavDropdown.Item href="#action/3.1">Illuminem</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Environmental Sustainability
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Energy</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Mining & Metals
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Social Responsability
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link className="mr-3" href="/">
+              Newsletter
+            </Nav.Link>
+
+            <NavDropdown
+              menuVariant="dark"
+              title="More"
+              className="mr-3"
+              id="collasible-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">
+                ESG Alliance
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Energy Converter
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Glossary</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Top 10</NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link className="mr-3" href="/">
+              About
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
