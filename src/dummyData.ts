25433de5b5a32b55af736b64190f1d375ac5e25c
JMTiburcio <jmrtme@gmail.com>
2023-04-24 19:42:08 -0300

    chore: add dummyData

---
+++ src/dummyData.ts
@@ -0,0 +1,109 @@
+interface News {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  color: string;
+  category: [string, string];
+}
+
+const data: News[] = [
+  {
+    id: 1,
+    title: "News 1",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-1.jpg",
+    color: "bg-primary",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 2,
+    title: "News 2",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-2.jpg",
+    color: "bg-secondary",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 3,
+    title: "News 3",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-3.jpg",
+    color: "bg-success",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 4,
+    title: "News 4",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-1.jpg",
+    color: "bg-danger",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 5,
+    title: "News 5",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-2.jpg",
+    color: "bg-warning",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 6,
+    title: "News 6",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-3.jpg",
+    color: "bg-info",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 7,
+    title: "News 7",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-1.jpg",
+    color: "bg-light",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 8,
+    title: "News 8",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-2.jpg",
+    color: "bg-dark",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 9,
+    title: "News 9",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-3.jpg",
+    color: "bg-primary",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 10,
+    title: "News 10",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-1.jpg",
+    color: "bg-secondary",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 11,
+    title: "News 11",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-2.jpg",
+    color: "bg-success",
+    category: ["category 1", "category 2"],
+  },
+  {
+    id: 12,
+    title: "News 12",
+    description: "Some quick example text to build on the card title",
+    image: "/images/image-3.jpg",
+    color: "bg-danger",
+    category: ["category 1", "category 2"],
+  },
+];
+
+export default data;
