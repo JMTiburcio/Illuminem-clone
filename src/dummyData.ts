86b47e5238b78812e1c5e4651b5f0bf670540b8e
JMTiburcio <jmrtme@gmail.com>
2023-04-26 20:49:29 -0300

    feat:  add type property and bigger description

--- src/dummyData.ts
+++ src/dummyData.ts
@@ -4,105 +4,130 @@ interface News {
   description: string;
   image: string;
   color: string;
-  category: [string, string];
+  type: string;
+  category: string[];
 }
 
 const data: News[] = [
   {
     id: 1,
     title: "News 1",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-1.jpg",
     color: "bg-primary",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 2,
     title: "News 2",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-2.jpg",
     color: "bg-secondary",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 3,
     title: "News 3",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-3.jpg",
     color: "bg-success",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 4,
     title: "News 4",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-1.jpg",
     color: "bg-danger",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 5,
     title: "News 5",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-2.jpg",
     color: "bg-warning",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 6,
     title: "News 6",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-3.jpg",
     color: "bg-info",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 7,
     title: "News 7",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-1.jpg",
     color: "bg-light",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 8,
     title: "News 8",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-2.jpg",
     color: "bg-dark",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 9,
     title: "News 9",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-3.jpg",
     color: "bg-primary",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 10,
     title: "News 10",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-1.jpg",
     color: "bg-secondary",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 11,
     title: "News 11",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-2.jpg",
     color: "bg-success",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
   {
     id: 12,
     title: "News 12",
-    description: "Some quick example text to build on the card title",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.",
     image: "/images/image-3.jpg",
     color: "bg-danger",
-    category: ["category 1", "category 2"],
+    type: "article",
+    category: ["category 1", "category 2", "category 3", "category 4"],
   },
 ];
 
